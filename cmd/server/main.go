bca94cfbe0c5ccb959993e413422bbd3d2b710b4
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2019-02-17 21:12:41 +0100

    renamed prometheus metrics

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,7 +18,7 @@ func main() {
 
 	counter := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "api_requests_total",
+			Name: "http_api_requests_total",
 			Help: "A counter for requests to the wrapped handler.",
 		},
 		[]string{"code", "method"},
@@ -28,7 +28,7 @@ func main() {
 	// buckets based on the expected request duration.
 	duration := prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "request_duration_seconds",
+			Name:    "http_request_duration_seconds",
 			Help:    "A histogram of latencies for requests.",
 			Buckets: []float64{.1, .3, .8, 2},
 		},
@@ -39,7 +39,7 @@ func main() {
 	// ObserverVec.
 	responseSize := prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:    "response_size_bytes",
+			Name:    "http_response_size_bytes",
 			Help:    "A histogram of response sizes for requests.",
 			Buckets: []float64{200, 500, 900, 1500},
 		},
